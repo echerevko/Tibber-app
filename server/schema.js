dae46b16dde1b30993701dbddaceb671879eca4e
Eugeniya Dzynia <evgeniya.cherevko@gmail.com>
2022-08-22 10:11:22 +0200

    Add server code

---
+++ server/schema.js
@@ -0,0 +1,30 @@
+const {buildSchema} = require('graphql')
+
+const schema = buildSchema(`
+    type Weather {
+        minTemperature: Float
+        maxTemperature: Float
+        entries: [Entrie]
+    }
+
+    type Entrie {
+        time: String
+        temperature: Float
+        type: String
+    }
+
+    type Query {
+        getWeather: Weather
+    }
+
+`)
+
+module.exports = schema
+
+// type Me{
+//     home(id: ID):Home
+// }
+
+// type Home{
+//     weather: Weather
+// }
